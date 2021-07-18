ed9adec49cd6ae39bbcca38996267b8fb86ee2a1
帅得乱七八糟 <284970670@qq.com>
2021-07-18 09:21:49 +0800

    使用babel处理ES6语法

--- study1/webpack.config.js
+++ study1/webpack.config.js
@@ -17,7 +17,26 @@ module.exports = {
     // hotOnly: true    // 表示即使热更新生效，也不让浏览器自动刷新
   },
   module: {
-    rules: [{
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,    // 因为第三方模块早就做好了转义，所以没必要再次使用一遍转义(exclude 是排除在外的意思)
+        use: {
+          loader: "babel-loader", 
+          options: {
+            presets: [['@babel/preset-env', {
+              "targets": {
+                "edge": "17",
+                "firefox": "60",
+                "chrome": "67",
+                "safari": "11.1"
+              },
+              useBuiltIns: 'usage'
+            }]]
+          }
+        }
+      },
+      {
       test: /\.(jpg|png|gif)$/,
       use: {
         loader: 'url-loader',
